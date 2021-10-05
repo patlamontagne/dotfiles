9a650640af2da3987df90118c41845a6a0905cef
Patrick Lamontagne <16765115+patlamontagne@users.noreply.github.com>
2021-10-05 12:17:10 +0200

    remove silent mode & try to remove lando-stable.dev before download

--- scripts/update-lando.sh
+++ scripts/update-lando.sh
@@ -1,7 +1,7 @@
 lando poweroff
 sudo service docker stop
 # download directly from github latest releases page.
-(cd ~ && curl -s https://api.github.com/repos/lando/lando/releases/latest \
+(cd ~ && rm lando-stable.deb -f && curl https://api.github.com/repos/lando/lando/releases/latest \
 | grep "browser_download_url.*deb" \
 | cut -d : -f 2,3 \
 | tr -d \" \
